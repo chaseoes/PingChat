b347a25f1e33e141be5d6d81cb30951f61a2232d
chaseoes <chase.oes@gmail.com>
2013-05-18 12:15:47 -0700

    Make the sound configurable.

--- src/main/java/me/chaseoes/pingchat/PingChat.java
+++ src/main/java/me/chaseoes/pingchat/PingChat.java
@@ -11,14 +11,22 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class PingChat extends JavaPlugin implements Listener {
     
     public void onEnable() {
+    	getConfig().options().copyDefaults(true);
+    	getConfig().options().header("PingChat by chaseoes. Go here for a list of sounds: http://jd.bukkit.org/rb/apidocs/org/bukkit/Sound.html");
+    	saveConfig();
         getServer().getPluginManager().registerEvents(this, this);
+    }
+    
+    public void onDisable() {
+    	reloadConfig();
+    	saveConfig();
     }
     
     @EventHandler(priority = EventPriority.MONITOR)
     public void onChat(AsyncPlayerChatEvent event) {
         for (Player player : event.getRecipients()) {
             if (event.getMessage().toLowerCase().contains(player.getName().toLowerCase())) {
-                player.playSound(player.getLocation(), Sound.ORB_PICKUP, 60f, 0f);
+                player.playSound(player.getLocation(), Sound.valueOf(getConfig().getString("sound").toUpperCase()), getConfig().getInt("volume"), getConfig().getInt("pitch"));
             }
         }
     }
